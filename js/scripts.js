aa567078fa65b59d64a461e5c260851eb46dde3e
Brenda Kiptim <kiptim54@gmail.com>
2018-02-13 05:58:50 +0300

    add subtract, divide and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,22 @@
 var add=function(x,y) {
 return x+y;
 };
+
+//subtract
+var subtract= function(x,y){
+  return x-y;
+};
+//multiply
+var muliply=function(x,y){
+  return x*y;
+};
+//divide
+var divide= function(x,y){
+  return x/y;
+};
+
 var x=parseInt(prompt("choose a number"));
 var y=parseInt(prompt("choose another number"));
-alert(add(x,y));
+
+var results=divide(x,y);
+alert(results);
